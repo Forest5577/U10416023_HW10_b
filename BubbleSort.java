08ff37d1ef12625f9529f2aeedb2f30f27b4e985
Forest5577 <Forest5757@gmail.com>
2016-06-06 18:31:06 +0800

    Set up the program type
    Add comment to the program and set up the type.

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,24 +1,36 @@
+//U10416023
+
+//Import the api we need
 import java.util.Scanner;
 
+//Main class
 class BubbleSort {
-  public static void main(String []args) {
-    int n, c, d, swap;
-    Scanner in = new Scanner(System.in);
-    //prompt the user
-    System.out.println("Input number of integers to sort");
-    n = in.nextInt();
-    //get the number of elements
-    int array[] = new int[n];
 
+  //Main method
+  public static void main(String []args) {
+
+    //Declare the variable we need
+    int n, c, d, swap;
+
+    //Create a scanner object by using the initial class
+    Scanner scanner = new Scanner(System.in);
+
+    //Asking the number input the user
+    System.out.println("Input number of integers to sort");
+    n = scanner.nextInt();
+
+    //Get the number of elements
+    int array[] = new int[n];
     System.out.println("Enter " + n + " integers");
+
     //input the n elements
     for (c = 0; c < n; c++)
-      array[c] = in.nextInt();
-    //using BubbleSort like C++
+      array[c] = scanner.nextInt();
+
+    //using BubbleSort
     for (c = 1; c < n; c++) {
       for (d = 0; d < n-1 ; d++) {
-        if (array[d] > array[d+1]) /* For descending order use < */
-        {
+        if (array[d] > array[d+1]){ /* For descending order use < */
           swap       = array[d];
           array[d]   = array[d+1];
           array[d+1] = swap;
@@ -26,8 +38,8 @@ class BubbleSort {
       }
     }
 
-    System.out.println("Sorted list of numbers");
     //show the outcome
+    System.out.println("Sorted list of numbers");
     for (c = 0; c < n; c++)
       System.out.println(array[c]);
   }
